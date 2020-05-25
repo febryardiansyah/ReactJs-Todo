db08bb9dc8b392b8187c3ec0011224e2f96cf801
febryardiansyah <febrymuhammad80@gmail.com>
2020-05-25 20:26:30 +0700

    Updates

---
+++ precache-manifest.e8c05e104f66d3c4571f6599f2e090d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990d6637ff7deb6f2c1df30667effab1",
+    "url": "/ReactJs-Todo/index.html"
+  },
+  {
+    "revision": "b80101c8344e69b4bcd2",
+    "url": "/ReactJs-Todo/static/css/main.a97cd0eb.chunk.css"
+  },
+  {
+    "revision": "97314f38fa48ad70cb43",
+    "url": "/ReactJs-Todo/static/js/2.6ca71cb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-Todo/static/js/2.6ca71cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80101c8344e69b4bcd2",
+    "url": "/ReactJs-Todo/static/js/main.db08e9de.chunk.js"
+  },
+  {
+    "revision": "8ade6b4da534850a2705",
+    "url": "/ReactJs-Todo/static/js/runtime-main.18a9756c.js"
+  }
+]);
